b649eb5aefa62bc3bad86f9719068595874d514c
Max Carrión <maxmateo.carrion@epn.edu.ec>
2024-11-18 10:52:52 -0500

    migrate: astro framework

---
+++ startquito.camaraemprendimiento.ec/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,astro}",
+    "node_modules/swiper/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
